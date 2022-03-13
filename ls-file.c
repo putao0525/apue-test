0c358abc9bb0e06d0ad8386286d4455c5dabf2a3
lizijie07 <>
2022-03-13 20:57:04 +0800

    uapte

--- ls-file.c
+++ ls-file.c
@@ -6,9 +6,9 @@ int main(int argc, char *argv[]) {
     DIR *dp;
     struct dirent *dirp;
     if (argc != 2)
-        err_quit("usage: ls directory_name");
+        err_quit("参数不合法，请输入目录");
     if ((dp = opendir(argv[1])) == NULL)
-        err_sys("can't open %s", argv[1]);
+        err_sys("不可以打开目录:%s", argv[1]);
     while ((dirp = readdir(dp)) != NULL)
         printf("%s\n", dirp->d_name);
     closedir(dp);
